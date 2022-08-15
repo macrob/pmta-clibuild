8980580b37c7eb55a60b77d6fe49b7835afd0ff3
Macrob <se@xambo.io>
2022-08-15 22:12:50 +0300

    add dns config

---
+++ dns-config.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+domain=`jq -r .domain config.json`;
+primaryIp=`jq -r .ip[0] config.json`;
+ips=`jq -r .ip[] config.json`;
+
+echo $primaryIp;
+tmpFile=./tmp/vm;
+pwd=`pwd`;
+
+###### CONFGIG CONST
+#uncomment next 1 lines;
+dkimDir = /etc/dkim;
+#dkimDir=./tmp/dkim/;
+
+
+#uncomment next 1 lines;
+zones=/etc/named/zones;
+#zones=./tmp/zones;
+
+
+rm -f $tmpFile;
+
+
+#### dkim
+echo "Генерация dkim"
+
+
+
+mkdir -p $dkimDir;
+
+dkimKeyPth="${dkimDir}/dkim5.${domain}.key";
+dkimPubPth="${dkimDir}/dkim5.${domain}.pub";
+
+openssl genrsa -out $dkimKeyPth 2048
+openssl rsa -in $dkimKeyPth -pubout > $dkimPubPth;
+chmod 0444 $dkimKeyPth;
+dkim=`cat $dkimPubPth | sed '/^-/d' | awk '{printf "%s", $1}'`
+dkimRecord=`echo "v=DKIM1; k=rsa; p=${dkim}" | sed -r 's/.{60}/&" "/g'`;
+
+zoneFile="${zones}/${domain}.zone"
+
+echo "zone \"${domain}\" IN {" >> $tmpFile
+echo "  type master;" >> $tmpFile
+echo "  file \"${zoneFile}\";" >> $tmpFile
+echo "  allow-update { none; };" >> $tmpFile
+echo "};" >> $tmpFile
+echo " " >> $tmpFile
+
+
+#uncomment next 2 lines;
+cat /tmp/vm >> /etc/named.conf
+rm -f $tmpFile;
+
+
+#####  ZONE FILE
+mkdir -p $zones;
+serial=`date +%Y%m%d00`
+
+#dkim=$(cat /etc/pmta/dkim/public_key_${domain[0]}.txt)
+touch $zoneFile;
+
+spfip=""
+for ip in $ips; do
+  spfip+="ip4:"${ip}" ";
+done
+
+cat > $zoneFile <<EOL
+\$ORIGIN .
+\$TTL 86400
+${domain}         IN      SOA             ns1.${domain}.        it.${domain}. (
+                                  ${serial} ; serial
+                                  300 ; refresh after 6 hours
+                                  600 ; retry after 1 hour
+                                  4800 ; expire after 1 week
+                                  86400 )         ; minimum TTL of 1 day
+
+                  IN    NS    ns1.${domain}.
+                  IN    NS    ns2.${domain}.
+
+                  IN    MX    10 mail.${domain}.
+ns1.${domain}.    IN    A    ${primaryIp}
+ns2.${domain}.    IN    A    ${primaryIp}
+${domain}.        IN    A    ${primaryIp}
+www.${domain}.    IN    A    ${primaryIp}
+smtp.${domain}.   IN    A    ${primaryIp}
+pop.${domain}.    IN    A    ${primaryIp}
+imap.${domain}.   IN    A    ${primaryIp}
+mail.${domain}.   IN    A    ${primaryIp}
+${domain}.        IN    TXT    "v=spf1 ${spfip} a mx ~all"
+dkim5._domainkey.${domain}.   IN    TXT    "$dkimRecord"
+_adsp._domainkey.${domain}.   IN    TXT    "dkim=all"
+_dmarc._domainkey.${domain}.  IN    TXT    "v=DMARC1; p=reject; adkim=s; aspf=s;"
+EOL
+
+for ip in $ips; do
+  while IFS='.' read -ra ipSplit; do
+    server="s${ipSplit[3]}";
+    echo "${server}.${domain}.    IN    A    ${ip}" >> $zoneFile
+  done <<< "$ip"
+done
+
+echo "Bind DNS install successful"
+
+systemctl enable named && systemctl restart named
---
+++ dns-install.sh
@@ -0,0 +1,33 @@
+
+echo "Установка Bind(named) DNS"
+yum -y install bind bind-utils >/dev/null 2>/dev/null
+mv /etc/named.conf /etc/named.conf.orig
+touch /etc/named.conf
+cat << 'EOF' > /etc/named.conf
+options {
+#       listen-on port 53 { 127.0.0.1; };
+#        listen-on-v6 port 53 { ::1; };
+        directory       "/var/named";
+        dump-file       "/var/named/data/cache_dump.db";
+        statistics-file "/var/named/data/named_stats.txt";
+        memstatistics-file "/var/named/data/named_mem_stats.txt";
+        allow-query     { any; };
+        allow-transfer  { localhost; };
+        recursion no;
+        dnssec-enable yes;
+        dnssec-validation yes;
+
+        /* Path to ISC DLV key */
+        bindkeys-file "/etc/named.iscdlv.key";
+
+        managed-keys-directory "/var/named/dynamic";
+};
+
+logging {
+        channel default_debug {
+                file "data/named.run";
+                severity dynamic;
+        };
+};
+EOF
+
