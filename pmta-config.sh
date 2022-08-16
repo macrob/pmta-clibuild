b3d0c11e05bc30cfddf43b26f65cc6bd97bd3544
Macrob <se@xambo.io>
2022-08-16 10:01:19 +0300

    fix

--- pmta-config.sh
+++ pmta-config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+domain=`jq -r .domain config.json`;
 
 ###### CONFGIG CONST
 dkimDir=/etc/dkim;
 dkimKeyPth="${dkimDir}/dkim5.${domain}.key";
 dkimPubPth="${dkimDir}/dkim5.${domain}.pub";
 
-domain=`jq -r .domain config.json`;
 echo "domain-key dkim5,$domain,${dkimKeyPth}" >> /etc/pmta/dkimlist.txt
 
 ./pmta-cli config-generate -s "/root/pmta-clibuild/pmta"
