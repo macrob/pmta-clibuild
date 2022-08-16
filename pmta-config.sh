d890b033a62bc62436cb505afda7162c77cb8ab7
Macrob <se@xambo.io>
2022-08-16 09:58:35 +0300

    add dkim

--- pmta-config.sh
+++ pmta-config.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+###### CONFGIG CONST
+dkimDir=/etc/dkim;
+dkimKeyPth="${dkimDir}/dkim5.${domain}.key";
+dkimPubPth="${dkimDir}/dkim5.${domain}.pub";
+
+domain=`jq -r .domain config.json`;
+echo "domain-key dkim5,$domain,${dkimKeyPth}" >> /etc/pmta/dkimlist.txt
+
 ./pmta-cli config-generate -s "/root/pmta-clibuild/pmta"
 
 cp -f ~/pmta-clibuild/build/config /etc/pmta/
